bde9c700eca8bb982c65ea636b3e42612b9d7cee
sunny-Ne5 <sunny.tiwari505@gmail.com>
2020-11-21 22:28:19 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask, render_template, redirect, url_for, request
+import waitress
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def trending():
+	msg={"message":"Hello from the other side"}
+	return json.dumps(msg)
+
+
+if __name__ == "__main__":
+     app.debug = False
+     port = int(os.environ.get('PORT', 33507))
+     waitress.serve(app, port=port)
